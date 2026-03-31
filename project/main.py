7796292d817e977b1fe9fea8ead359bd24ff7947
Akshit Awasthi <awasthiakshit02@gmail.com>
2026-03-31 19:16:53 +0530

    Add CRUD logic and /refresh endpoint for database insertion

--- project/main.py
+++ project/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
-from database import engine, Base
+from database import engine, Base, SessionLocal
 from parser import load_all_products
+from crud import create_or_update_product
 
 app = FastAPI()
 
@@ -12,10 +13,20 @@ def root():
     return {"message": "API is running"}
 
 
-@app.get("/test-load")
-def test_load():
+@app.get("/refresh")
+def refresh():
+    db = SessionLocal()
     products = load_all_products()
-    return {
-        "count": len(products),
-        "sample": products[:2]
+
+    result = {
+        "created": 0,
+        "updated": 0,
+        "no_change": 0
     }
+
+    for p in products:
+        status = create_or_update_product(db, p)
+        result[status] += 1
+
+    db.close()
+    return result
